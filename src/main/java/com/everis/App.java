eaafb4c2ca892db662ed8df637c7ceb98da22cdc
= <=>
2022-02-11 08:01:52 +0100

    Añadiendo reto Joda Time

---
+++ src/main/java/com/everis/App.java
@@ -0,0 +1,18 @@
+package com.everis;
+
+import org.joda.time.LocalDate;
+
+
+public class App {
+ 
+
+	 public static void main(String[] args) {
+
+		 System.out.println("La fecha actual es: " + LocalDate.now());
+	       
+	       
+	    }
+ 
+    
+
+}
